61fff5c11a526f6c7d3707126996d6b9ee16f48c
Daniel Pisfil <pisfilzd@gmail.com>
2021-03-22 23:11:31 -0500

    Docs agregado

---
+++ docs/precache-manifest.25bc54ae7540af1bf395fafa6ce42a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850256d05f2253bc2be0b822f38e81a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b7959918b85654072f",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b7959918b85654072f",
+    "url": "/static/js/main.5e003e26.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
